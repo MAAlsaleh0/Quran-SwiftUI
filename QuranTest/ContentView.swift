d1df1ad36e138ec12457768b9770119cce1abc92
Mohammed Alsaleh <Malsalehh0@gmail.com>
2022-04-03 16:08:23 -0700

    Initial Commit

---
+++ QuranTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuranTest
+//
+//  Created by Mohammed Alsaleh on 02/09/1443 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuranTest/QuranTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuranTestApp.swift
+//  QuranTest
+//
+//  Created by Mohammed Alsaleh on 02/09/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct QuranTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
